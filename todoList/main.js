641b907037ebec763798c36c9fb63c489136a28b
Marcos Martilotta <marcos.martilotta@gmail.com>
2022-02-21 16:48:45 -0300

    avancé un poco con la todolist

--- todoList/main.js
+++ todoList/main.js
@@ -1,3 +1,5 @@
+const todos = []
+
 window.onload = () => { 
     const form = document.getElementById('todo-form'); //Me posiciono en el formulario
     form.onsubmit = (e) => { //creo una fat function que con onsubmit toma el comportamiento del boton
@@ -5,7 +7,10 @@ window.onload = () => {
         const todo = document.getElementById('todo'); //Tomo lo que escribo en el input
         const todoText = todo.value; //Tomo el texto que tiene
         todo.value = ''; //Borro lo que dice en el texto
-        console.log(todoText)
+        todos.push(todoText); //Pusheo a la lista todos el contenido de totoText
+        const todoList = document.getElementById('todo-list');
+        todoList.innerHTML = ''; //vacío la lista cada vez que agrego un elemento
+
     }
 }
 
